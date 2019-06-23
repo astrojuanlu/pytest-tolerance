6fe0f03c0823016d6102467e16a24a910ae59fe6
Juan Luis Cano Rodríguez <juanlu001@gmail.com>
2019-06-23 19:09:15 +0200

    Add stateful fixture
    Missing:
    
    * Proper bisection
    * Better reporting
    * Storage of tolerances

--- pytest_tolerance.py
+++ pytest_tolerance.py
@@ -1,12 +1,16 @@
 """
-Large parts of this code are inspired by
-https://github.com/pytest-dev/pytest-rerunfailures/
+General acknowledgements:
+
+* Large parts of this code are inspired by
+  https://github.com/pytest-dev/pytest-rerunfailures/
+* See also https://github.com/pytest-dev/pytest/issues/3261#issuecomment-369740536
+  for a useful representation of the hook order
+
 """
 
 import random
 
 import pytest
-
 from _pytest.runner import runtestprotocol
 
 
@@ -19,8 +23,8 @@ def pytest_configure(config):
 
 def pytest_runtest_protocol(item, nextitem):
     """Controls how tests are rerun."""
-    marked = item.get_closest_marker("tolerance")
-    if marked is None:
+    marker = item.get_closest_marker("tolerance")
+    if marker is None:
         return
 
     need_to_run = True
@@ -55,7 +59,91 @@ def pytest_report_teststatus(report):
         return "rerun", "R", ("RERUN", {"yellow": True})
 
 
+@pytest.hookimpl(hookwrapper=True, tryfirst=True)
+def pytest_runtest_makereport(item, call):
+    # https://github.com/pytest-dev/pytest/issues/230#issuecomment-223453793
+    outcome = yield
+    rep = outcome.get_result()
+    setattr(item, "rep_" + rep.when, rep)
+    return rep
+
+
+FIXTURES_CACHE = {}
+
+
+def get_fixture(nodeid, *args, **kwargs):
+    if nodeid not in FIXTURES_CACHE:
+        FIXTURES_CACHE[nodeid] = ToleranceFixture(*args, **kwargs)
+
+    return FIXTURES_CACHE[nodeid]
+
+
+class ToleranceFixture:
+    def __init__(
+        self, try_exact=True, min_exponent=-20, max_exponent=0, random_values=False
+    ):
+        self._try_exact = try_exact
+        self._random_values = random_values
+        self._min_exponent = min_exponent
+        self._max_exponent = max_exponent
+
+        self._last_exponent = None
+        self._num_calls = 0
+        self._last_failed = None
+
+    def _get_next_exponent_bisection(self):
+        # TODO: This is promising but currently useless
+        # Once the tests passes we never call the fixture again
+        # so we don't have the opportunity
+        # to give a more accurate exponent
+        if self._last_exponent is None:
+            self._last_exponent = (self._min_exponent + self._max_exponent) // 2
+
+        if self._last_failed:
+            # Relax the tolerance
+            return (self._last_exponent + self._max_exponent) // 2
+        else:
+            # Tighten the tolerance
+            return (self._last_exponent + self._min_exponent) // 2
+
+    def _get_next_exponent_step(self, step=1):
+        # TODO: This can potentially make the tests very slow
+        if self._last_exponent is None:
+            self._last_exponent = self._min_exponent
+
+        return self._last_exponent + step
+
+    def __call__(self):
+        if self._random_values:
+            # Just return a random value
+            value = 10 ** random.randint(self._min_exponent, self._max_exponent)
+
+        elif self._try_exact and self._num_calls == 0:
+            # Let's first try the exact comparison
+            value = 0.0
+
+        else:
+            self._last_exponent = self._get_next_exponent_step()
+            value = float(10 ** self._last_exponent)
+
+        self._num_calls += 1
+        print(value)
+        return value
+
+    def set_last_failed(self, last_failed):
+        self._last_failed = last_failed
+
+
 @pytest.fixture(scope="function")
 def tolerance(request):
-    value = 1 * 10 ** random.randint(-20, 0)
-    return value
+    item = request.node
+    marker = item.get_closest_marker("tolerance")
+
+    if marker is None:
+        raise ValueError("Mark the test with @pytest.mark.tolerance()")
+    else:
+        fixture = get_fixture(item.nodeid, *marker.args, **marker.kwargs)
+
+    yield fixture()
+
+    fixture.set_last_failed(item.rep_call.failed)
